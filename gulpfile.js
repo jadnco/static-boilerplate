5bef031c9135ad2baa7fd01cb8582cdf60f36292
Jaden Dessureault <jaden.dessureault@gmail.com>
2015-08-18 15:58:28 -0500

    Add src and dist path object

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,36 @@ var gulp        = require('gulp'),
     ghPages     = require('gulp-gh-pages'),
     sassGlob    = require('gulp-sass-bulk-import');
 
-var dist = './dist';
+var paths = {
+  src: { root: 'src' },
+  dist: { root: 'dist' },
+  init: function() {
+    this.src.sass        = this.src.root + '/scss/main.scss';
+    this.src.templates   = this.src.root + '/*.hbs';
+    this.src.javascript  = [this.src.root + '/js/**/*.js','!' + this.src.root + '/js/libs/*.js'];
+    this.src.libs        = this.src.root + '/js/libs/*.js';
+    this.src.images      = this.src.root + '/images/**/*.{jpg,jpeg,svg,png,gif}';
+    this.src.files       = this.src.root + '/*.{html,txt}';
+
+    this.dist.css        = this.dist.root + '/css';
+    this.dist.images     = this.dist.root + '/images';
+    this.dist.javascript = this.dist.root + '/js';
+    this.dist.libs       = this.dist.root + '/js/libs';
+    
+    return this;
+  }
+}.init();
 
 gulp.task('serve', function() {
   browserSync.init({
-    server: dist,
+    server: paths.dist.root,
     open: false,
     notify: false
   });
 });
 
 gulp.task('styles', function() {
-  gulp.src(['src/scss/main.scss'])
+  gulp.src([paths.src.sass])
     .pipe(sassGlob())
     .on('error', util.log)
     .pipe(sass({
@@ -31,7 +49,7 @@ gulp.task('styles', function() {
     .on('error', util.log)
     .pipe(prefixer('last 2 versions'))
     .on('error', util.log)
-    .pipe(gulp.dest(dist + '/css/'))
+    .pipe(gulp.dest(paths.dist.css))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -44,14 +62,14 @@ gulp.task('templates', function() {
     batch: ['src/partials']
   };
 
-  gulp.src(['src/*.hbs'])
+  gulp.src([paths.src.templates])
     .pipe(handlebars(null, opts))
     .on('error', util.log)
     .pipe(rename({
       extname: '.html'
     }))
     .on('error', util.log)
-    .pipe(gulp.dest(dist))
+    .pipe(gulp.dest(paths.dist.root))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -59,54 +77,54 @@ gulp.task('templates', function() {
 * Bundle all javascript files
 */
 gulp.task('scripts', function() {
-  gulp.src(['src/js/**/*.js', '!src/js/libs/*.js'])
+  gulp.src(paths.src.javascript)
     .pipe(concat('bundle.js'))
     .on('error', util.log)
     .pipe(uglify())
     .on('error', util.log)
-    .pipe(gulp.dest(dist + '/js/'));
+    .pipe(gulp.dest(paths.dist.javascript));
 
   /*
   * Uglify JS libs and move to dist folder
   */
-  gulp.src(['src/js/libs/*.js'])
+  gulp.src([paths.src.libs])
     .pipe(uglify())
     .on('error', util.log)
     .pipe(rename({
       suffix: '.min'
     }))
     .on('error', util.log)
-    .pipe(gulp.dest(dist + '/js/libs'));
+    .pipe(gulp.dest(paths.dist.libs));
 });
 
 gulp.task('images', function() {
-  gulp.src(['src/images/**/*.{jpg,jpeg,svg,png,gif}'])
-    .pipe(gulp.dest(dist + '/images'));
+  gulp.src([paths.src.images])
+    .pipe(gulp.dest(paths.dist.images));
 });
 
 gulp.task('clean:images', function(a) {
-  del([dist + '/images/**/*.{jpg,jpeg,svg,png,gif}'], a);
+  del([paths.dist.images], a);
 });
 
 gulp.task('files', function() {
-  gulp.src(['src/*.{html,txt}'])
-    .pipe(gulp.dest(dist));
+  gulp.src([paths.src.files])
+    .pipe(gulp.dest(paths.dist.root));
 });
 
 gulp.task('clean:files', function(a) {
-  del([dist + '/*.{html,txt}'], a);
+  del([paths.dist.root + '/*.{html,txt}'], a);
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', ['styles']);
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch('src/**/*.hbs', ['templates']);
-  gulp.watch('src/*.{html,txt}', ['clean:files', 'files']);
-  gulp.watch('src/images/**/*.{jpg,jpeg,svg,png,gif}', ['clean:images', 'images']);
+  gulp.watch(paths.src.sass, ['styles']);
+  gulp.watch(paths.src.javascript, ['scripts']);
+  gulp.watch(paths.src.templates, ['templates']);
+  gulp.watch(paths.src.files, ['clean:files', 'files']);
+  gulp.watch(paths.src.images, ['clean:images', 'images']);
 });
 
 gulp.task('deploy', function() {
-  return gulp.src([dist + '/**/*'])
+  return gulp.src([paths.dist.root + '/**/*'])
     .pipe(ghPages());
 });
 
